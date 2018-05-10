54769632d6c1e79c71fca4b0df6d36628d71b9ac
tanyongkai <tanyongkai@100tal.com>
2018-05-10 12:53:10 +0800

    修改页面和MovieDao

--- src/main/java/com/zhazha/sx/dao/MovieDao.java
+++ src/main/java/com/zhazha/sx/dao/MovieDao.java
@@ -14,24 +14,35 @@ public class MovieDao {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    public List<Movies> getMovies(){
-        String sql = "SELECT id,name,PerformerId,MainPerformerName,hasMosaic,releaseTime,torrent FROM movie";
-        return jdbcTemplate.query(sql,new Object[]{},new BeanPropertyRowMapper<Movies>(Movies.class));
+    public List<Movies> getMovies() {
+        return getMovies(1,20);
+    }
+
+    public List<Movies> getMovies(int pageNum, int rowNum) {
+        String sql = "SELECT id,name,PerformerId,MainPerformerName,hasMosaic,releaseTime,torrent FROM movie limit ?,?";
+        return jdbcTemplate.query(sql, new Object[]{}, new BeanPropertyRowMapper<Movies>(Movies.class));
 
     }
 
-    /**
-             return (List<TestBean>) jdbcTemplate.query(sql, new RowMapper<TestBean>(){
-                 @Override
-                 public TestBean mapRow(ResultSet rs, int rowNum) throws SQLException {
-                     TestBean stu = new TestBean();
-                     stu.setId(rs.getInt("ID"));
-                     stu.setName(rs.getString("NAME"));
-                     return stu;
-                 }
+    public List<Movies> selectMovies(int pageNum, int rowNum) {
+        String sql = "SELECT id,name,PerformerId,MainPerformerName,hasMosaic,releaseTime,torrent FROM movie limit ?,?";
+        return jdbcTemplate.query(sql, new Object[]{}, new BeanPropertyRowMapper<Movies>(Movies.class));
 
-             }
-             );
+    }
+
+
+
+    /**
+     return (List<TestBean>) jdbcTemplate.query(sql, new RowMapper<TestBean>(){
+    @Override public TestBean mapRow(ResultSet rs,int pageNum,int rowNum) throws SQLException {
+    TestBean stu = new TestBean();
+    stu.setId(rs.getInt("ID"));
+    stu.setName(rs.getString("NAME"));
+    return stu;
+    }
+
+    }
+     );
      */
 
 }
